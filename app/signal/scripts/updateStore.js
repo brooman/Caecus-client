ee1aaa73969c30080c2d607350e1a1aee54febf8
André Broman <bromanandre@gmail.com>
2019-11-11 14:19:24 +0100

    Fixed bug with store

--- app/signal/scripts/updateStore.js
+++ app/signal/scripts/updateStore.js
@@ -1,5 +1,14 @@
 export default store => {
   return `;(() => {
-    window.SignalStore.store = formatInput(JSON.parse('${store}'))
+    window.SignalStore.store = formatInput(JSON.parse(atob('${store}')))
+
+    if(window.SignalStore.store !== {}) {
+      Object.keys(window.SignalStore.store).map((item) => {
+        if (/^(session)/.test(item)) {
+          window.SignalStore.store[item] = JSON.parse(window.SignalStore.store[item])
+        }
+      })
+    }
+
   })();`
 }
